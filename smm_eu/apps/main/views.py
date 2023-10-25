37235076c2fbfd6a029c1c9c52034b65f5239d65
XOTTABY4 <xottab2013@gmail.com>
2023-10-25 02:36:56 +0000

    ALPHA

--- smm_eu/apps/main/views.py
+++ smm_eu/apps/main/views.py
@@ -11,11 +11,12 @@ def instagramfol(request):
     list_inst_fol = Products.objects.filter(type=1)
     return render(request, 'main/instagramfol.html', {'list_inst_fol': list_inst_fol})
 
-
+##################################################################################################################
+######################################INSTAGRAME BUY VIEWS########################################################
+##################################################################################################################
 def instagramlike(request):
     list_inst_fol = Products.objects.filter(type=1)
     return render(request, 'main/instagramlike.html', {'list_inst_fol': list_inst_fol})
-
 
 def instagramfolbuy(request, price_id):
     try:
@@ -23,6 +24,51 @@ def instagramfolbuy(request, price_id):
     except:
         raise Http404('Not Found!')
     return render(request, 'main/instagramfolbuy.html', {'order_by_id_fol': order_by_id_fol})
+
+def instagramlikebuy(request):
+    list_inst_like = Products.objects.filter(type=2)
+    return render(request, 'main/buy/instagramlikebuy.html', {'list_inst_like': list_inst_like})
+##################################################################################################################
+##################################################################################################################
+
+def tiktokfol(request):
+    list_tik_tok_fol = Products.objects.filter(type=6)
+    return render(request, 'main/tiktokfol.html', {'list_tik_tok_fol': list_tik_tok_fol})
+
+def tiktoklike(request):
+    list_tik_tok_like = Products.objects.filter(type=5)
+    return render(request, 'main/tiktoklike.html', {'list_tik_tok_like': list_tik_tok_like})
+
+def tiktokviews(request):
+
+    list_tik_tok_views = Products.objects.filter(type=7)
+    return render(request, 'main/tiktokviews.html', {'list_tik_tok_views': list_tik_tok_views})
+##################################################################################################################
+##########################################TIKTOK BUY VIEWS########################################################
+##################################################################################################################
+
+def tiktokfolbuy(request, price_id):
+    try:
+        order_by_id_fol = Products.objects.get(id=price_id)
+    except:
+        raise Http404('Not Found!')
+    return render(request, 'main/tiktokfolbuy.html', {'order_by_id_fol': order_by_id_fol})
+
+def tiktoklikebuy(request, price_id):
+    try:
+        order_by_id_fol = Products.objects.get(id=price_id)
+    except:
+        raise Http404('Not Found!')
+    return render(request, 'main/tiktoklikebuy.html', {'order_by_id_fol': order_by_id_fol})
+
+def tiktokviewsbuy(request, price_id):
+    try:
+        order_by_id_fol = Products.objects.get(id=price_id)
+    except:
+        raise Http404('Not Found!')
+    return render(request, 'main/tiktokviewsbuy.html', {'order_by_id_fol': order_by_id_fol})
+##################################################################################################################
+##################################################################################################################
 
 
 def order(request, price_id):
@@ -36,27 +82,5 @@ def order(request, price_id):
     return render(request, 'main/order.html', {'order_by_id_fol': order_by_id_fol, 'type': type})
 
 
-def buy100like(request):
-    list_inst_fol = Products.objects.filter(type=1)
-    return render(request, 'main/buy/buy-100-likes-on-instagram.html', {'list_inst_fol': list_inst_fol})
 
-
-def buy500like(request):
-    list_inst_fol = Products.objects.filter(type=1)
-    return render(request, 'main/buy/buy-500-likes-on-instagram.html', {'list_inst_fol': list_inst_fol})
-
-
-def buy1000like(request):
-    list_inst_fol = Products.objects.filter(type=1)
-    return render(request, 'main/buy/buy-1000-likes-on-instagram.html', {'list_inst_fol': list_inst_fol})
-
-
-def buy2500like(request):
-    list_inst_fol = Products.objects.filter(type=1)
-    return render(request, 'main/buy/buy-2500-likes-on-instagram.html', {'list_inst_fol': list_inst_fol})
-
-
-def buy5000like(request):
-    list_inst_fol = Products.objects.filter(type=1)
-    return render(request, 'main/buy/buy-5000-likes-on-instagram.html', {'list_inst_fol': list_inst_fol})
 
